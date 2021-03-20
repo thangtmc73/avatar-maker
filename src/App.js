f34436ba5f45bd4fd475f928f0f279651aee5ddf
thangtmc73 <thangtm.dev@gmail.com>
2021-03-21 00:20:37 +0700

    feat: add main content in main view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
+import PreviewView from "./components/preview-view/PreviewView";
 
 function App() {
   return (
     <div className="app">
+      <div className="main">
+        <PreviewView />
+      </div>
     </div>
   );
 }
