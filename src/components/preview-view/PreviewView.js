f2523a1a381d27c04b72e37fada1bcd55288d1c3
thangtmc73 <thangtm.dev@gmail.com>
2021-03-21 00:27:01 +0700

    feat: show avatar in PreviewView with hard code data

--- src/components/preview-view/PreviewView.js
+++ src/components/preview-view/PreviewView.js
@@ -1,11 +1,22 @@
 import "./PreviewView.css";
+import Item from "./Item";
+import { ItemType, ItemID } from "../../consts";
 
 function PreviewView() {
   return (
     <div className="preview-view">
-      
+      <Item type={ItemType.Hair} id={ItemID.LongCurlyHair} />
+      <Item type={ItemType.Eyebrows} id={ItemID.SadEyebrows} />
+      <Item type={ItemType.Eyes} id={ItemID.CryEyes} />
+      <Item type={ItemType.Accesories} id={ItemID.Earing1Accesories} />
+      <Item type={ItemType.Tattoos} id={ItemID.ThroatTattoos} />
+      <Item type={ItemType.FacialHair} id={ItemID.FancyFacialHair} />
+      <Item type={ItemType.Mouth} id={ItemID.SmileMouth} />
+      <Item type={ItemType.Body} id={ItemID.DefaultBody} />
+      <Item type={ItemType.Glasses} id={ItemID.NerdGlasses} />
+      <Item type={ItemType.Clothes} id={ItemID.HoodieClothes} />
     </div>
-  )
+  );
 }
 
 export default PreviewView;
