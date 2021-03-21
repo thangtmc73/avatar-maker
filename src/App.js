bba297c533f2c2b3e5ba24b0b8b045d8f4260dc2
thangtmc73 <thangtm.dev@gmail.com>
2021-03-21 12:40:00 +0700

    feat: show ListCategories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import PreviewView from "./components/preview-view/PreviewView";
+import ListCategories from "./components/list-categories/ListCategories";
 
 function App() {
   return (
     <div className="app">
       <div className="main">
         <PreviewView />
+        <ListCategories />
       </div>
     </div>
   );
