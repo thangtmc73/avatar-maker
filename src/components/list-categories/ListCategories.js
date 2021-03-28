822295716a86e3daa98c8eae7a4d2d09d5095c0e
thangtmc73 <thangtm.dev@gmail.com>
2021-03-28 15:04:03 +0700

    feat: handle select category to dispatch action update

--- src/components/list-categories/ListCategories.js
+++ src/components/list-categories/ListCategories.js
@@ -1,23 +1,32 @@
 import Config from "../../config";
 import "./ListCategories.css";
-import { useSelectedCategory } from "../../AppContext";
+import {
+  useSelectedCategory,
+  useDispatch,
+  updateCategory,
+} from "../../AppContext";
 
 const { category_order, category_info } = Config;
 const listCategories = category_order;
 
 function ListCategories() {
   const selectedCategoryId = useSelectedCategory();
+  const dispatch = useDispatch();
   return (
     <div className="list-categories">
       {listCategories.map((categoryId) => {
         const category = category_info[categoryId] || {};
         const { name, key } = category;
+        function handleClick() {
+          dispatch(updateCategory(categoryId));
+        }
         return (
           <div
             className={`category ${
               selectedCategoryId === categoryId ? "selected" : ""
             }`}
             key={key}
+            onClick={handleClick}
           >
             <span>{name}</span>
           </div>
